59ca102227fc2d14a71a72378cdd7d737316126e
NhatNguyen <minhnhat.nguyen@ntucenterprise.sg>
2024-12-06 18:49:04 +0800

    Trigger

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nhat2008.github.io',
-	// base: 'nhat-resume',
+	base: '/',
 	outDir: 'dist',
 
 	integrations: [
